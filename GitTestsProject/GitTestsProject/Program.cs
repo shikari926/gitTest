db22768f3746b8f6af4e33c0eca65b01936648c6
Mikoleso <mikoleso@gmail.com>
2022-05-14 19:14:48 +0600

    Добавлена функция умножения

--- GitTestsProject/GitTestsProject/Program.cs
+++ GitTestsProject/GitTestsProject/Program.cs
@@ -2,19 +2,29 @@
 
 namespace GitTestsProject
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             int a = 3;
             int b = 4;
+            Console.WriteLine(multiply(a, b));
         }
+
 
         //Максим добавить метод возведения числа в степень б
 
 
         //Миша - метод умножения всех числел от а до б
+        public static int multiply(int a, int b) {
+            int c = 1;
+            while (a <= b) {
+                c = c * a;
+                a++;
+            }
+            return c;
+        }
 
         // Костя - добавить метод какое из числе меньше а или б
 
