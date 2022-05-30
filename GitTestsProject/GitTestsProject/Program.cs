a7d8e72c5a6e192dc44943fd11311c9bb7ead70b
TAIGO <TAIGO@CYBERCHOPA>
2022-05-30 21:13:10 +0600

    Test

--- GitTestsProject/GitTestsProject/Program.cs
+++ GitTestsProject/GitTestsProject/Program.cs
@@ -8,19 +8,29 @@ namespace GitTestsProject
         { 
             Random random = new Random();
             var x = random.Next(100);
-
+            int a = 4;
+            int b = 3;
             var text = Console.ReadLine();
+            Console.WriteLine(Max(a, b));
         }
 
-        //Максим добавить метод возведения числа в степень б
-
+        //Максим добавить метод возведения числа в степень б\\
 
         //Миша - метод умножения всех числел от а до б
 
         // Костя - добавить метод какое из числе меньше а или б
 
         // Тая - добавить метод поиска большего сдели а и б
-
+        private static int Max(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else if (a < b)
+                return b;
+            else
+                return 0;
+        }
         // Артем -  добавить метод вывода а и б на консоль
+
     }
 }
